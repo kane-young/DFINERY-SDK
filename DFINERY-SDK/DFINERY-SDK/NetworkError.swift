a40461ece5f5113f395f172b067cd9de9b25a5e8
kane-young <lyw2100@naver.com>
2022-04-25 15:26:57 +0900

    [#2] NetworkError 타입 생성 및 구현

---
+++ DFINERY-SDK/DFINERY-SDK/NetworkError.swift
@@ -0,0 +1,31 @@
+//
+//  NetworkError.swift
+//  DFINERY-SDK
+//
+//  Created by 이영우 on 2022/04/25.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+  case invalidRequest
+  case connectionProblem
+  case invalidResponseStatusCode(_ statusCode: Int)
+  case invalidData
+  case decodingError
+
+  var message: String {
+    switch self {
+    case .invalidRequest:
+      return "유효하지 않은 요청"
+    case .connectionProblem:
+      return "네트워킹 문제"
+    case .invalidResponseStatusCode(let statusCode):
+      return "상태 코드 \(statusCode)"
+    case .invalidData:
+      return "반환 데이터 미존재"
+    case .decodingError:
+      return "디코딩 에러"
+    }
+  }
+}
