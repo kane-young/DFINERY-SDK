f773f458e51847d7f792d07c41bdcb5bfe2ceb02
kane-young <lyw2100@naver.com>
2022-04-25 15:23:01 +0900

    [#2] HTTPMethod 타입 생성 및 구현

---
+++ DFINERY-SDK/DFINERY-SDK/HTTPMethod.swift
@@ -0,0 +1,19 @@
+//
+//  HTTPMethod.swift
+//  DFINERY-SDK
+//
+//  Created by 이영우 on 2022/04/25.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+  case get = "GET"
+  case put = "PUT"
+  case post = "POST"
+  case delete = "DELETE"
+  case head = "HEAD"
+  case connect = "CONNECT"
+  case options = "OPTIONS"
+  case patch = "PATCH"
+}
