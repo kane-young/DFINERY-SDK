3577e13a2112ae05bb38ca0be73cfa59ae145911
kane-young <lyw2100@naver.com>
2022-04-27 09:40:32 +0900

    [#17] 위치 정보 설정

--- DfineryAnalyticsDemo/DfineryAnalyticsDemo/AppDelegate.swift
+++ DfineryAnalyticsDemo/DfineryAnalyticsDemo/AppDelegate.swift
@@ -5,32 +5,70 @@
 //  Created by 이영우 on 2022/04/27.
 //
 
+import CoreLocation
 import UIKit
+
+import DfineryAnalytics
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+  private let locationManager = CLLocationManager()
+  private let igaSDK = IGASDK.getInstance
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+
+    self.igaSDK.initIGASDK(appKey: "ss@naver.com")
+    self.igaSDK.setUserProperties(with: [
+      "gender": "male",
+      "age": 28,
+      "name": "youngwoo Lee"
+    ])
+
+    self.locationManager.delegate = self
+    self.requestLocationUsagePermission()
+
     return true
   }
 
   // MARK: UISceneSession Lifecycle
 
   func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
     return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
   }
-
-  func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-  }
-
-
 }
 
+extension AppDelegate: CLLocationManagerDelegate {
+  func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+    print("location error is = \(error.localizedDescription)")
+  }
+
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    self.setLocation()
+  }
+
+  func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+    switch manager.authorizationStatus {
+    case .authorizedAlways, .authorizedWhenInUse:
+      self.setLocation()
+      print("Location 설정")
+    case .restricted, .notDetermined:
+      print("GPS 권한 설정되지 않음")
+    case .denied:
+      print("GPS 권한 요청 거부됨")
+    default:
+      print("GPS: Default")
+    }
+  }
+
+  func requestLocationUsagePermission() {
+    self.locationManager.requestWhenInUseAuthorization()
+  }
+
+  func setLocation() {
+    if let coordinate = self.locationManager.location?.coordinate {
+      self.igaSDK.setLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+    }
+  }
+}
