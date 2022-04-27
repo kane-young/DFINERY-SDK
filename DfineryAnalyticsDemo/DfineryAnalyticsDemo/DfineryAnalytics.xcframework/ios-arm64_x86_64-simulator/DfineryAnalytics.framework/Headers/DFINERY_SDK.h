96b0e6ba29243f0ab0b1c71a698e5325745204f3
kane-young <lyw2100@naver.com>
2022-04-27 09:16:59 +0900

    [#17] SDK 추가

---
+++ DfineryAnalyticsDemo/DfineryAnalyticsDemo/DfineryAnalytics.xcframework/ios-arm64_x86_64-simulator/DfineryAnalytics.framework/Headers/DFINERY_SDK.h
@@ -0,0 +1,16 @@
+//
+//  DFINERY_SDK.h
+//  DFINERY-SDK
+//
+//  Created by 이영우 on 2022/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DFINERY_SDK.
+FOUNDATION_EXPORT double DFINERY_SDKVersionNumber;
+
+//! Project version string for DFINERY_SDK.
+FOUNDATION_EXPORT const unsigned char DFINERY_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DFINERY_SDK/PublicHeader.h>
