0bc7daebc8b369ade03c591d5e2bb7889f3d0a1d
kane-young <lyw2100@naver.com>
2022-04-27 09:20:45 +0900

    [#17] IDFA 설정

--- DfineryAnalyticsDemo/DfineryAnalyticsDemo/ViewController.swift
+++ DfineryAnalyticsDemo/DfineryAnalyticsDemo/ViewController.swift
@@ -5,11 +5,42 @@
 //  Created by 이영우 on 2022/04/27.
 //
 
+import AdSupport
+import AppTrackingTransparency
 import UIKit
 
-class ViewController: UIViewController {
+import DfineryAnalytics
+
+final class ViewController: UIViewController {
+
+  private let igaSDK = IGASDK.getInstance
 
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+
+    self.requestPermission()
+  }
+
+  func requestPermission() {
+    ATTrackingManager.requestTrackingAuthorization { status in
+      switch status {
+      case .authorized:
+        self.igaSDK.startGettingIDFA()
+        let idfa = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+        self.igaSDK.setAppleAdvertisingIdentifier(idfa)
+      case .denied:
+        self.igaSDK.stopGettingIDFA()
+      case .notDetermined:
+        self.igaSDK.stopGettingIDFA()
+      case .restricted:
+        self.igaSDK.stopGettingIDFA()
+      @unknown default:
+        self.igaSDK.stopGettingIDFA()
+      }
+    }
   }
 }
